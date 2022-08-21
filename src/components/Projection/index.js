06381e14d824a576c8a08ce2e642228e5144d1d5
SB379 <sidbalaji03@gmail.com>
2022-08-21 18:18:35 -0400

    Further Updates @ 6:18 PM

--- src/components/Projection/index.js
+++ src/components/Projection/index.js
@@ -13,7 +13,6 @@ const Projection = () => {
                         Here, I was given autonomy to develop a Real-Time Projection Mapping system to create real holograms from the ground up. This daunting task enabled me to take my Python knowledge and apply advanced concepts in Computer Vision using OpenCV
                         and other such libraries.
                     </p>
-                    <img src = {Temp}/>
                 </div>
                 <Link to = "/projects" class = 'flat-button'>Back</Link>
             </div>
